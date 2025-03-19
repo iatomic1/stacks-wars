a08d2f11bbc2a0c0ccf287371be03a0543a1e60d
Flames <isaacomenuche@gmail.com>
2025-03-19 19:39:14 +0100

    feat: restructure LexiWars game components and remove legacy files
    - Deleted outdated single-player and multiplayer lobby components.
    - Introduced a new LexiWars component with enhanced gameplay mechanics and multiplayer support.
    - Updated routing for joining games to reflect new structure.
    - Improved game state management and user feedback through toast notifications.

---
+++ src/app/lexi-wars/[[...id]]/page.tsx
@@ -0,0 +1,32 @@
+import LexiWars from "@/components/lexi-wars/lexi-wars";
+
+interface PlayerScore {
+	username: string;
+	score: number;
+	isCurrentPlayer?: boolean;
+}
+
+export default async function LobbyPlayGround({
+	params,
+}: {
+	params: Promise<{ id: string }>;
+}) {
+	const { id } = await params;
+	console.log(id[0]);
+
+	let oppsData: PlayerScore[] = [];
+
+	if (id[0] === "1") {
+		oppsData = [
+			{ username: "You", score: 10, isCurrentPlayer: true },
+			{ username: "WordWeaver", score: 42 },
+			{ username: "LexiconLegend", score: 38 },
+			{ username: "VocabVirtuoso", score: 35 },
+			{ username: "WordWeaver", score: 42 },
+			{ username: "LexiconLegend", score: 38 },
+			{ username: "VocabVirtuoso", score: 35 },
+		];
+	}
+
+	return <LexiWars oppsData={oppsData} />;
+}
