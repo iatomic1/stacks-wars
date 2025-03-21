92f5a3a3df5c2afefeda2dd79be94b29c4d62046
adeyemialameen04 <atomic.k.2739@gmail.com>
2025-03-21 02:08:11 +0100

    feat: implemented games services

--- src/types/lobbySchema.ts
+++ src/types/lobbySchema.ts
@@ -1,47 +1,47 @@
 export interface Game {
-	id: string;
-	createdAt: string;
-	updatedAt: string;
-	name: string;
-	description: string;
-	tags: string[];
-	image: string | null;
-	activeLobbies: number;
-	totalPrize: number;
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  name: string;
+  description: string;
+  tags: string;
+  image: string | null;
+  activeLobbies: number;
+  totalPrize: string;
 }
 
 export interface Pool {
-	id: string;
-	createdAt: string;
-	updatedAt: string;
-	lobbyId: string;
-	currentAmount: number;
-	targetAmount: number;
-	maxPlayers: number;
-	entryAmount: number;
-	contractId: string | null;
-	deployContractTxId: string;
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  lobbyId: string;
+  currentAmount: number;
+  targetAmount: number;
+  maxPlayers: number;
+  entryAmount: number;
+  contractId: string | null;
+  deployContractTxId: string;
 }
 
 interface Participant {
-	id: string;
-	stxAddress: string;
-	username: string;
-	ready: boolean;
-	amount: number;
+  id: string;
+  stxAddress: string;
+  username: string;
+  ready: boolean;
+  amount: number;
 }
 
 export interface Lobby {
-	id: string;
-	createdAt: string;
-	updatedAt: string;
-	name: string;
-	creator: Participant;
-	gameId: string;
-	maxPlayers: number;
-	status: "pending" | "open" | "completed" | "cancelled";
-	description: string;
-	game: Game;
-	pool: Pool;
-	participants: Participant[];
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  name: string;
+  creator: Participant;
+  gameId: string;
+  maxPlayers: number;
+  status: "pending" | "open" | "completed" | "cancelled";
+  description: string;
+  game: Game;
+  pool: Pool;
+  participants: Participant[];
 }
