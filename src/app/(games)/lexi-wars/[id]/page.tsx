708930a38bd1a82317fbe0cd595549045d8f75fa
Flames <isaacomenuche@gmail.com>
2025-04-09 00:12:02 +0100

    refactor: remove checkparticipant api route

--- src/app/(games)/lexi-wars/[id]/page.tsx
+++ src/app/(games)/lexi-wars/[id]/page.tsx
@@ -1,13 +1,15 @@
+import { getLobbyParticipants } from "@/lib/services/participants";
 import Content from "./_components/content";
 
 export default async function LobbyPlayGround({
-  params,
+	params,
 }: {
-  params: Promise<{ id: string }>;
+	params: Promise<{ id: string }>;
 }) {
-  const { id } = await params;
-  if (id) console.log(id);
-  else console.log("No id");
+	const { id } = await params;
+	if (id) console.log(id);
+	else console.log("No id");
+	const participants = await getLobbyParticipants(id);
 
-  return <Content id={id} />;
+	return <Content participants={participants} id={id} />;
 }
